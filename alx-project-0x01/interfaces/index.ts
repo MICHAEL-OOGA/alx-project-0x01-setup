eff5911f4b9ce2b2edfff8a2fa5109e2d60c2b29
unknown <manofsteel1544@gmail.com>
2025-07-20 10:32:14 +0300

    update

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -70,6 +70,5 @@ export interface UserData {
 export interface UserModalProps {
   isOpen: boolean;
   onClose: () => void;
-
-  onSubmit: (newUser: UserData) => void;
+  onSubmit: (post: UserProps) => void;
 }
